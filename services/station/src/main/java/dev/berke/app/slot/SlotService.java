f0fc4089e18ed89ba9d06db36c027ca6534e2419
Berke Sayın <sayinberke34@gmail.com>
2024-12-12 16:33:39 +0300

    Set up redis to cache responses for frequently accessed endpoints

--- services/station/src/main/java/dev/berke/app/slot/SlotService.java
+++ services/station/src/main/java/dev/berke/app/slot/SlotService.java
@@ -2,6 +2,8 @@ package dev.berke.app.slot;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,17 +16,21 @@ public class SlotService {
     private final SlotRepository slotRepository;
     private final SlotMapper slotMapper;
 
+
+    @CachePut(value = "slots", key = "#result")
     public Integer createSlot(SlotRequest slotRequest) {
         var slot = slotMapper.toSlot(slotRequest);
         return slotRepository.save(slot).getId();
     }
 
+    @Cacheable(value = "slots", key = "#slotId")
     public SlotResponse getSlotById(Integer slotId) {
         return slotRepository.findById(slotId)
                 .map(slotMapper::toSlotResponse)
                 .orElseThrow(() -> new EntityNotFoundException("Slot not found with the ID:: " + slotId));
     }
 
+    @Cacheable(value = "slots")
     public List<SlotResponse> getAllSlots() {
         return slotRepository.findAll()
                 .stream()
